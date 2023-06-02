0ac0815cefcc104cce9e805dbb8f6d80f15828c3
Taislaine Massucato <massucato2@outlook.com>
2023-06-02 16:47:49 -0300

    Inclusão de Repositories e melhorias nas escritas de código

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using ProjeAcessoDadosDapper;
+
+namespace Blog.Repositories
+{
+    public class UserRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public UserRepository(SqlConnection connection)
+            => _connection = connection;
+
+          public IEnumerable<User> Get()
+            => _connection.GetAll<User>();        
+
+           public User Get(int id)
+            =>  _connection.Get<User>(id);   
+
+           public void Create(User user)
+             =>  _connection.Insert<User>(user);        
+        
+    }
+}
